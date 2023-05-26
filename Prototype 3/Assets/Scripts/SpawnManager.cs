05ba71594bf2ed3df4b7bd1d80392b2f882f2522
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-05-26 09:03:24 +0700

    Update SpawnManager.cs
    1. Change startDelay time, 3s to 5s.
    2. Change spawnTime 2.0s to 2.5s.
    
    Please correct and test.

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,8 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePreb;
     public Vector3 spawnPos = new Vector3(25, 0, 0);
-    public float startDelay = 2.0f;
-    public float repeatRate = 2.0f;
+    public float startDelay = 5.0f;
+    public float repeatRate = 2.5f;
 
     public Playercontroller playerCOntrollerScript;
 
